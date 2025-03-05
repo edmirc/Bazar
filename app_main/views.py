10ada46b016d9ed96df6e373bb1acda0d38f5495
edmirc <edmirc@gmail.com>
2025-03-05 11:46:31 -0400

    aprimoramento do detail view

--- app_main/views.py
+++ app_main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product, Photo
 from django.db.models import Prefetch
 
@@ -12,6 +12,25 @@ class InitiView(ListView):
     def get_context_data(self, **kwargs):
         context=super().get_context_data(**kwargs)
         context['product'] = context['product'].filter(sold=False)
-        dici: dict = dict()
-        context['image'] = Photo.objects.filter(cover=True)
+        context['image'] = Photo.objects.filter(cover=True, sold=False)
         return context
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search = self.request.GET.get('busca')
+        if search:
+            queryset = queryset.filter(name__icontains=search, sold=False)
+            print(queryset)
+            return queryset
+        return queryset
+    
+
+class ProductDetailView(DetailView):
+    template_name = 'detail.html'
+    model = Product
+    context_object_name = 'product'
+    queryset = Product.objects.prefetch_related(Prefetch('foto', queryset=Photo.objects.all()))
+    
+   
+
+    
